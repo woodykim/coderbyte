69f9a3062edebcedbc8e73607d59d7a3804203b9
Woody Kim <woodykorea@gmail.com>
2015-07-30 19:31:43 +0900

    Create ex_oh.rb

---
+++ ex_oh.rb
@@ -0,0 +1,15 @@
+def ExOh(str)
+  x = 0
+  o = 0
+      
+  str.each_char do |c|
+    x += 1 if c == "x"
+    o += 1 if c == "o"
+  end
+                    
+  return x == o ? "true" : "false"
+end
+      
+# keep this function call here 
+# to see how to enter arguments in Ruby scroll down   
+ExOh(STDIN.gets) 
